c21cc6abebca2679a651d60aa9d70629bbf724d5
Anthony Jesmok <anthony@jesmok.xyz>
2023-08-28 22:18:15 -0500

    Add Components and Style Folders

---
+++ components/layout/NavHeader.jsx
@@ -0,0 +1,11 @@
+import { Header, HeaderName } from '@carbon/react';
+
+export default function NavHeader() {
+  return (
+    <Header aria-label="Anthony Jesmok">
+      <HeaderName href="#" prefix="">
+        Anthony Jesmok
+      </HeaderName>
+    </Header>
+  );
+}
